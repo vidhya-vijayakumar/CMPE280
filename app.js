f3ab893d882942f8d5894aac8c5ab4f33d3108e0
haroon <haroon@pinger.com>
2018-12-09 16:33:10 -0800

    changed port number

--- app.js
+++ app.js
@@ -94,4 +94,8 @@ app.use('/users', users);
 
 module.exports = app;
 require('./app-server/routes/note.routes.js')(app);
-app.listen(3000);
+app.set( 'port', ( process.env.PORT || 5000 ));
+// Start node server
+app.listen( app.get( 'port' ), function() {
+  console.log( 'Node server is running on port ' + app.get( 'port' ));
+  });
